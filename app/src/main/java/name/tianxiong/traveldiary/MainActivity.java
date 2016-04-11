ac0850a5b7049f8612acaf16f5aa80240a9ef719
Tianxiong <yangtianxiong1991@gmail.com>
2016-04-11 16:49:26 -0400

    build toolbar

---
+++ app/src/main/java/name/tianxiong/traveldiary/MainActivity.java
@@ -0,0 +1,49 @@
+package name.tianxiong.traveldiary;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    Window window;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //set status bar color
+        window = getWindow();
+        window.setStatusBarColor(Color.BLACK);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.menu_item_new_diary:
+                return true;
+            case R.id.menu_item_login:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
